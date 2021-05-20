6636a3d44dc1f7955aaa72836c3b223713533fb8
Udofia Udo <me@udofiaudo.com>
2021-05-21 00:25:48 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// console.log("he, world");
+
+// function add(num1, num2){
+//     return num1 * num2;
+// }
+
+// console.log(add(10, 5));
+
+const naira = 410.83;
+
+function currencyConvert(dollar){
+    return dollar * naira;
+}
+
+console.log(`One dollar is equivalent of N${currencyConvert(2)}.`);
